5a065dae64223a261f422f0a0ce7fde49a9bd26d
unknown <09021382328171@student.unsri.ac.id>
2024-11-27 14:25:16 +0700

    db lagi

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+// Specify views directory (default is './views')
+app.set('views', './views');
+
+app.use(express.static('public')); // Untuk static files seperti CSS, JS, gambar
+
 app.get('/', (req, res) => {
     res.render('index'); // Halaman utama
 });
